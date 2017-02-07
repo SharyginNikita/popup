b16f74b051c3ed359a53df987a088e4ea400d6a9
Igor Bykov <igorb.dev@websm.io>
2017-02-07 15:41:29 +0300

    refresh project

--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,33 @@ const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const concatCss = require('gulp-concat-css');
-const urlAdjuster = require('gulp-css-url-adjuster');
 const babel = require('gulp-babel');
 const jsmin = require('gulp-jsmin');
 const sourcemaps = require('gulp-sourcemaps');
 
-//Задачи по-умолчанию
 gulp.task('default', [
-	'build-min-sm.css',
-	'build-min-js',
+	'build-css',
+	'build-js',
 ]);
 
-//Следит за изменением файлов в каталоге
 gulp.task('watch', () => {
-	gulp.watch('src/scss/*.scss', ['build-min-sm.css']);
-	gulp.watch('src/js/*.js', ['build-min-js']);
+	gulp.watch('./src/scss/*.scss', ['build-css']);
+	gulp.watch('./src/js/*.js', ['build-js']);
 });
 
-//Создает карту файла, преобразует *.scss в *.css и минифицирует
-gulp.task('build-min-sm.css', () => {
-	gulp.src('src/scss/*.scss')
+gulp.task('build-css', () => {
+	gulp.src('./src/scss/*.scss')
 		.pipe(sourcemaps.init())
 		.pipe(sass())
 		.pipe(autoprefixer())
 		.pipe(cleanCSS())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(sourcemaps.write('maps'))
-		.pipe(gulp.dest('public/css'))
+		.pipe(gulp.dest('./public'))
 });
 
-//собирает и минифицирует js, прогоняет через babel
-gulp.task('build-min-js', () => {
-	gulp.src('src/js/*.js')
+gulp.task('build-js', () => {
+	gulp.src('./src/js/*.js')
 		.pipe(sourcemaps.init())
 		.pipe(babel({
 			presets: ['latest']
@@ -43,5 +38,5 @@ gulp.task('build-min-js', () => {
 		.pipe(jsmin())
 		.pipe(rename({suffix: '.min'}))
 		.pipe(sourcemaps.write('maps'))
-		.pipe(gulp.dest('public/js'))
+		.pipe(gulp.dest('./public'))
 });
